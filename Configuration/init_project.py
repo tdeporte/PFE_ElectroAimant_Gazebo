ce80451c08e10ad02f2a75c3b52b43ed0ed6efab
Jessy <jessy.nolibe@etu.u-bordeaux.fr>
2022-03-22 21:40:21 +0100

    Update: Configuration

--- Configuration/init_project.py
+++ Configuration/init_project.py
@@ -10,13 +10,7 @@ def copy_file(current , new):
     remove_file(new + current)
     shutil.copy(current , new)
     
-def copy_dir(current , new):
-    if(os.path.exists(new + "/" + current)):
-        shutil.rmtree(new + "/" + current)
-    
-    shutil.copytree(current , new + "/" + current)
-    
-def copy_dir2(main_dir , current , new):
+def copy_dir(main_dir , current , new):
     if( os.path.exists(new + "/" + current) ):
         shutil.rmtree(new + "/" + current)
         
@@ -25,31 +19,40 @@ def copy_dir2(main_dir , current , new):
 def remove_file(file):
     if( os.path.exists(file) ):
         os.remove(file)
+        
+def copy_all(dir , dst):
+    files = os.listdir(dir)
+    
+    for i in files:
+        if( os.path.isdir( dir + i) ):
+            copy_dir(dir , i , dst)
+        else:
+            copy_file(dir + i , dst)
 
 if __name__ == '__main__':
-    px4_path = sys.argv[1]
-    
-    world_path_destination = px4_path + "Tools/sitl_gazebo/worlds/"
-    launch_path_destination = px4_path + "launch/"
-    models_path_destination = px4_path + "Tools/sitl_gazebo/models/"
-    iris_path_destination = models_path_destination + "iris/"
-    
-    world_path_current = "worlds/"
-    launch_path_current = "launch/"
-    models_path_current = "models/"
+    if( len(sys.argv) == 2 ):
+        px4_path = sys.argv[1]
         
-    copy_file(launch_path_current + "custom.launch" , launch_path_destination)
+        sitl_path_destination = px4_path + "Tools/sitl_gazebo/"
+        world_path_destination = sitl_path_destination + "worlds/"
+        launch_path_destination = px4_path + "launch/"
+        models_path_destination = sitl_path_destination + "models/"
+        iris_path_destination = models_path_destination + "iris/"
+        
+        world_path_current = "worlds/"
+        launch_path_current = "launch/"
+        models_path_current = "models/"
+        
+        copy_all(launch_path_current , launch_path_destination)
+        
+        copy_file(models_path_current + "iris.sdf" , iris_path_destination)
+        copy_file(models_path_current + "iris_base.xacro" , models_path_destination + "rotors_description/urdf/")
+        copy_dir(models_path_current , "plate" , models_path_destination)
+        
+        copy_all(world_path_current , world_path_destination)
+        copy_all("Library/" , sitl_path_destination)
+    else:
+        print("One argument required ! The argument must be the path to the PX4-Autopilot folder")
     
-    copy_file(models_path_current + "iris.sdf" , iris_path_destination)
-    copy_file(models_path_current + "iris_base.xacro" , models_path_destination + "rotors_description/urdf/")
-    copy_dir2(models_path_current , "plate" , models_path_destination)
     
-    copy_file(world_path_current + "PFE.world" , world_path_destination)
-    
-    dirs = os.listdir('Library/')
-    for i in dirs:
-        if( os.path.isdir( "Library/" + i ) ):
-            copy_dir2("Library/" , i, px4_path + "Tools/sitl_gazebo")
-        else:
-            copy_file("Library/" + i , px4_path + "Tools/sitl_gazebo")
     
