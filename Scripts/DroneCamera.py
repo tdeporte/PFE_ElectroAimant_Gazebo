773876b67174ada49585918abd9f503255a3924a
Jessy <jessy.nolibe@etu.u-bordeaux.fr>
2022-03-25 10:51:39 +0100

    Update: DroneCamera add utils functions

--- Scripts/DroneCamera.py
+++ Scripts/DroneCamera.py
@@ -18,6 +18,13 @@ class DroneCamera:
     def image_callback(self, img):
       self.bridge = CvBridge()
       self.cv_image = self.bridge.imgmsg_to_cv2(img)
+      
+      if(self.stream_camera == True):
+        cv2.imshow("CameraWindow", cv2.cvtColor(self.cv_image,cv2.COLOR_BGR2RGB))
+        cv2.waitKey(1)
+      else:
+        cv2.destroyAllWindows()
+        cv2.waitKey(1)
          
     def get_center_QR_code(self):
       qrDecoder = cv2.QRCodeDetector()
@@ -27,5 +34,23 @@ class DroneCamera:
         return center
       else:
           return None,None
+        
+    def get_image(self):
+      self.current_image = self.cv_image
+      rospy.loginfo("Get camera image")
+      
+    def save_image(self, filename):
+      self.current_image = self.cv_image
+      cv2.imwrite(filename , self.current_image)
+      rospy.loginfo("Save camera image")  
+    
+    def start_stream(self):
+      self.stream_camera = True
+      rospy.loginfo("Stream started")
+      
+    def stop_stream(self):
+      self.stream_camera = False
+      rospy.loginfo("Stream stopped")
+    
 
     
